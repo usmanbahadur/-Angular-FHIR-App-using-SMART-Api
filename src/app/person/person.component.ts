98d42e87b5ba04ed9cc11a5f91c732d42c85cd3e
Usman Bahadur <usman.bahadur@gosh.nhs.uk>
2021-10-18 11:52:27 +0100

    Commit

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,78 +1,78 @@
-import {Component, Input, OnInit, OnDestroy} from '@angular/core';
-import {PersonService} from './person.service';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
-import { Subscription, Subject } from 'rxjs';
-import {map} from 'rxjs/operators';
-import { PARENT_INJECTOR } from '@angular/core/src/render3/interfaces/injector';
+import { Component, Input, OnInit, OnDestroy } from "@angular/core";
+import { PersonService } from "./person.service";
+import { Subscription, Subject } from "rxjs";
 
-@Component ({
-selector: 'app-person',
-templateUrl: './person.component.html'
-
+@Component({
+  selector: "app-person",
+  templateUrl: "./person.component.html",
 })
-
 export class PersonComponent implements OnInit, OnDestroy {
+  personList;
+  PatAddress;
+  PatMRN;
+  PatGender;
+  PatTelecom;
+  DateOfBirth;
+  BAIRTHDATE;
+  PatCond;
+  Condition;
+  ConditionVal;
+  ObservationCode;
+  ObservationVal;
+  isFetching = false;
 
-personList ;
-PatAddress;
-PatMRN;
-PatGender;
-PatTelecom;
-DateOfBirth;
-BAIRTHDATE;
-PatCond;
-CondStat;
-Condverf;
-isFetching = false;
-private perSubList: Subscription;
-constructor(private prsService: PersonService) {
-}
+  private perSubList: Subscription;
+  constructor(private prsService: PersonService) {}
 
-ngOnInit() {
+  ngOnInit() {
+    this.prsService.fetchPerson();
 
-
-  this.prsService.fetchPerson();
-
-  this.perSubList = this.prsService.PatientName.subscribe(person => {
-    this.personList = person.slice (0, 15);
-    this.isFetching = false;
-  });
-  this.perSubList = this.prsService.PateintAddress.subscribe(address => {
-    this.PatAddress = address;
-  });
-  this.perSubList = this.prsService.Dob.subscribe(identifier => {
-    this.PatMRN = identifier.slice(17, 60);
-  });
-
-  this.perSubList = this.prsService.Dob.subscribe(gender => {
-  this.PatGender = gender.slice(11, 17);
-  });
-
-  this.perSubList = this.prsService.Dob.subscribe(birthDate => {
-  this.DateOfBirth = birthDate.slice (0, 10);
-  });
-
-
-  this.perSubList = this.prsService.PatientName.subscribe(telecom => {
-  this.PatTelecom = telecom.slice(16, 50);
-  });
-  this.perSubList = this.prsService.PatientCond.subscribe(PatientCond => {
-  this.PatCond = PatientCond. slice(16, 250);
+    this.perSubList = this.prsService.PatientName.subscribe((person) => {
+      this.personList = person.slice(0, 15);
+      this.isFetching = false;
+    });
+    this.perSubList = this.prsService.PateintAddress.subscribe((address) => {
+      this.PatAddress = address;
+    });
+    this.perSubList = this.prsService.Dob.subscribe((identifier) => {
+      this.PatMRN = identifier.slice(17, 60);
     });
 
-  this.perSubList = this.prsService.PatientCond.subscribe(CondStatus => {
-  this.CondStat = CondStatus.slice (0, 10);
-  });
+    this.perSubList = this.prsService.Dob.subscribe((gender) => {
+      this.PatGender = gender.slice(11, 17);
+    });
 
-  this.perSubList = this.prsService.PatientCond.subscribe(CondVerf => {
-  this.Condverf = CondVerf.slice (10, 16);
-  });
+    this.perSubList = this.prsService.Dob.subscribe((birthDate) => {
+      this.DateOfBirth = birthDate.slice(0, 10);
+    });
 
-  this.isFetching = true;
-}
+    this.perSubList = this.prsService.PatientName.subscribe((telecom) => {
+      this.PatTelecom = telecom.slice(16, 50);
+    });
 
-ngOnDestroy()
-{
-this.perSubList.unsubscribe();
-}
+    this.perSubList = this.prsService.PatientCondition.subscribe(
+      (Pcondition) => {
+        this.Condition = JSON.stringify(Pcondition, ["clinicalStatus"]["code"]);
+      }
+    );
+    this.perSubList = this.prsService.PatientCondition.subscribe(
+      (PconditionVal) => {
+        this.ConditionVal = JSON.stringify(PconditionVal, ["text"]);
+      }
+    );
+
+    this.perSubList = this.prsService.PatientObs.subscribe((PObservation) => {
+      this.ObservationVal = JSON.stringify(PObservation, ["value"]);
+    });
+
+    this.perSubList = this.prsService.PatientObs.subscribe((PObservation) => {
+      this.ObservationCode = JSON.stringify(PObservation, ["code"]);
+    });
+
+    this.isFetching = true;
+  }
+
+  ngOnDestroy() {
+    this.perSubList.unsubscribe();
+  }
 }
--- src/app/person/person.service.ts
+++ src/app/person/person.service.ts
@@ -1,71 +1,143 @@
-import {Injectable} from '@angular/core';
-import {Subject} from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import {map} from 'rxjs/operators';
-import { registerLocaleData } from '@angular/common';
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
-
-@Injectable ({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class PersonService {
-
-  PatientName = new Subject <string>();
-  PateintAddress = new Subject <string[]>();
-  PatientMrn = new Subject <string[]>();
-  PatientCond = new Subject <string>();
-  PatientMed = new Subject <string[]>();
-  Gender = new Subject <string[]>();
-  teleCom = new Subject <string[]>();
-  Dob = new Subject <string>();
+  PatientName = new Subject<string>();
+  PateintAddress = new Subject<string[]>();
+  PatientMrn = new Subject<string[]>();
+  PatientObs = new Subject<string>();
+  PatientCondition = new Subject<[]>();
+  Gender = new Subject<string[]>();
+  teleCom = new Subject<string[]>();
+  Dob = new Subject<string>();
   person: string[];
-  address: string [];
+  address: string[];
   identifier: string[];
   birth: string[];
-  CondStatus = new Subject <string>();;
-
-
-
+  CondStatus = new Subject<string>();
+  code: {};
   constructor(private http: HttpClient) {}
 
   fetchPerson() {
+    const FHIRClient = this.http.get<any>(
+      "https://r4.smarthealthit.org/Patient/87a339d0-8cae-418e-89c7-8651e6aab3c6"
+    );
 
-
-   const FHIRClient =  this.http.get<any>('https://r3.smarthealthit.org/Patient/smart-2113340')
-
-   // Get Patient Deomographics
-
-   FHIRClient.pipe(map(resData => {
-    return resData.name.map ((GivenName: {family: any,  given: any}) => GivenName.family + " " + GivenName.given)
-     + " " + resData.telecom.map ((Telecom: {value: any}) => Telecom.value);
-    }) ).subscribe(transformedData => {
+    FHIRClient.pipe(
+      map((resData) => {
+        return (
+          resData.name.map(
+            (GivenName: { family: any; given: any }) =>
+              GivenName.family + " " + GivenName.given
+          ) +
+          " " +
+          resData.telecom.map((Telecom: { value: any }) => Telecom.value)
+        );
+      })
+    ).subscribe((transformedData) => {
       this.PatientName.next(transformedData);
     });
 
-   FHIRClient.pipe(map(resData => {
-    return resData.address.map((Address: { use: any; line: any; city: any; state: any; postalCode: any; country : any;})=>
-    Address.use + " " +  Address.line  +" "+ Address.city +" "+ Address.state +" "+ Address.postalCode +" "+ Address.country);
-    })).subscribe(transformedData => {
-     this.PateintAddress.next(transformedData);
+    FHIRClient.pipe(
+      map((resData) => {
+        return resData.address.map(
+          (Address: {
+            use: any;
+            line: any;
+            city: any;
+            state: any;
+            postalCode: any;
+            country: any;
+          }) =>
+            Address.use +
+            " " +
+            Address.line +
+            " " +
+            Address.city +
+            " " +
+            Address.state +
+            " " +
+            Address.postalCode +
+            " " +
+            Address.country
+        );
+      })
+    ).subscribe((transformedData) => {
+      this.PateintAddress.next(transformedData);
     });
 
-   FHIRClient.pipe(map(resData => {
-    return resData.birthDate + ' ' + resData.gender + " " + resData.identifier[0].value; }))
-    .subscribe(transformedData => {
-    this.Dob.next((transformedData));
+    FHIRClient.pipe(
+      map((resData) => {
+        return (
+          resData.birthDate +
+          " " +
+          resData.gender +
+          " " +
+          resData.identifier[0].value
+        );
+      })
+    ).subscribe((transformedData) => {
+      this.Dob.next(transformedData);
     });
 
+    // Get Patient Vital Sign Observation
 
-    // Get Patient Condition
-   this.http.get<any>('https://r3.smarthealthit.org/Condition/smart-Condition-178')
-    .pipe(map(resData => {
-      return  resData.verificationStatus + " " + resData.clinicalStatus + " " +
-      resData.code.coding.map((Condition: {display: any; }) => Condition.display);
-    }))
-    .subscribe(transformedData =>
-     { this.PatientCond.next(transformedData);
-    });
+    this.http
+      .get<any>(
+        "https://r4.smarthealthit.org/Observation?patient=87a339d0-8cae-418e-89c7-8651e6aab3c6"
+      )
+      .pipe(
+        map((resData) => {
+          var Observation = resData.entry;
+          console.log(Observation);
+          for (var c = 0; c < Observation.length; c++)
+            return Observation.map(
+              (Observation: {
+                resource: any;
+                valueQuantity: any;
+                value: any;
+              }) => Observation ['resource']['valueQuantity']
+            );
+        })
+      )
+      .subscribe((transformedData) => {
+        this.PatientObs.next((transformedData));
+        console.log(transformedData);
+      });
 
+    this.http
+      .get<any>(
+        "https://r4.smarthealthit.org/Condition?patient=87a339d0-8cae-418e-89c7-8651e6aab3c6"
+      )
+      .pipe(
+        map((resData) => {
+          var Condition = resData.entry;
+          console.log(Condition);
+          for (var c = 0; c < Condition.length; c++)
+            return Condition.map(
+              (Condition: {
+                resource: any;
+                clinicalStatus: any;
+                code: any;
+                coding: any;
+                display: any;
+              }) => Condition.resource['code']
+            )
+     
+
+        })
+      )
+      .subscribe((transformedData) => {
+        this.PatientCondition.next(transformedData);
+        console.log(transformedData);
+      });
   }
-
-
-
 }
+//for (var product of products) {
+//  console.log(product.product_desc)
+//}
+//resultArr.forEach(AllDate => dateArr.push({date: AllDate.date}));
+//console.log(dateArr);
